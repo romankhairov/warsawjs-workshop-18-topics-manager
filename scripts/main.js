3edba9dd67e313f16abe1d7507aa698d38684faf
Roman <roman.khairov@gmail.com>
2018-03-18 13:49:32 +0100

    add menu

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+require ('bulma');
+
+
+//const $login = document.querySelector(".js-login-via-github");
+//$login.onclick = function () {
+//        console.log("Test");
+//    };
+//
+//document.querySelector(".js-login-via-github");
+// onclick = function () {
+//        console.log("Test 2");
+//    };
+
+const $login = document.querySelector(".js-login-via-github");
+$login.onclick = () => {
+        console.log("Test");
+    };
