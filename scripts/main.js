2a0191277d2bce8770b60f6d12267aa4a7a4395f
Roman <roman.khairov@gmail.com>
2018-03-18 15:56:17 +0100

    add user info

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
 require ('bulma');
+const hello = require('hellojs');
 
+hello.init({
+    github: 'c8917518020c7fc8f6a8'
+});
 
 //const $login = document.querySelector(".js-login-via-github");
 //$login.onclick = function () {
@@ -12,6 +16,53 @@ require ('bulma');
 //    };
 
 const $login = document.querySelector(".js-login-via-github");
-$login.onclick = () => {
-        console.log("Test");
-    };
+const $user = document.querySelector(".js-user");
+
+$login.addEventListener('click', (e) => {
+
+    e.preventDefault();
+    hello('github').login()
+        .then(function () {
+            return hello('github').api('/me');
+        })
+        .then(function (userProfile) {
+            console.log(userProfile.login);
+            console.log(userProfile.avatar_url);
+            renderUserDetails(userProfile)
+        })
+
+});
+
+function renderUserDetails(userProfile) {
+    const template = `
+        <div class="navbar-item">
+            ${userProfile.login}
+    
+            <img src="${userProfile.avatar_url}" alt=""/>
+        </div>  
+`
+    $user.innerHTML += template;
+    $user.innerHTML += template;
+    
+}
+
+
+
+
+//const $logout = document.querySelector(".js-logout-via-github");
+
+//$logout.addEventListener('click', (e) => {
+//    
+//        e.preventDefault();
+//        hello('github').login()
+//        .then(function () {
+//            return hello('github').api('/me');
+//        })
+//        .then(function (userProfile) {
+//            console.log(userProfile);
+//        })
+//                
+//    });
+
+
+
