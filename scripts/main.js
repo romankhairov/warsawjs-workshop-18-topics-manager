4839ebf28192e9f30966c1defe6e3a31a49d54e3
Roman <roman.khairov@gmail.com>
2018-03-23 20:40:17 +0100

    finish the main tasks

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,25 @@
-require ('bulma'); // css framework
+require ('bulma'); // 1) css framework
 
 
-const hello = require('hellojs'); // external package
+const hello = require('hellojs'); // 2) external package for auth
 const config = require('./config');// make data more organized with external package
 
 const $form = document.querySelector(".js-form-add-topic"); //$+name - is for quicker indicate of query obj
 const $login = document.querySelector(".js-login-via-github");
 const $logout = document.querySelector(".js-logout-via-github");
 const $user = document.querySelector(".user");
+const $jstopics = document.querySelector(".jstopics");
 
 
+// 12) prevent the lack of data
+const userAuthCheck = hello('github').getAuthResponse();
 
-hello.init({
-    github: config.GITHUB.CLIENT_ID
-});
+if ( userAuthCheck !== null) {  
+    renderUser();  
+}
 
-
-const getUserData = () => {
+ 
+function renderUser(){
     
     hello('github').api('/me')
         .then(function (userProfile) {
@@ -25,54 +28,84 @@ const getUserData = () => {
     
 };
 
-const userAuthCheck = hello('github').getAuthResponse();
-
-// check for login only once
-if ( userAuthCheck !== null) {
-    
-    getUserData(); 
-    
-}
 
 
-
-$login.addEventListener('click', (e) => {
-    console.log("login");
-    e.preventDefault();
-    hello('github').login()
-        .then(function () { 
-            getUserData();
-        
+// 3) config hellojs
+hello.init({
+    github: config.GITHUB.CLIENT_ID
 });
-   
+
+
+
+$login.addEventListener('click', (evt) => { 
+    evt.preventDefault(); // 5) turn off default act links 
+    hello('github').login() // 4) log in
+        // 6) function for automatic getting profile
+        .then(function () {  
+            return hello('github').api('/me');
+        })  
+            // 7) listener of getting user prof
+            .then((userProfile) => renderUserDetails(userProfile));
+                
+});
+
+
+
+// 9) template 
+function renderUserDetails(userProfile) {
+    const template = ` 
+        <div class="navbar-item">
+            ${userProfile.login}
+            ${userProfile.location}
     
+            <img src="${userProfile.avatar_url}" alt=""/>
+        </div>  
+`
+    // 10) insert html element
+    $user.innerHTML += template;
+    
+};
 
 
+$logout.addEventListener('click', () => {
+        console.log("logout");
+        hello.logout('github')
+            .then(() => location.reload(true)) // 11) refresh page after logout
+    });
+
+
+
+
+
+
+// 14) write "in-memory storage" data 
 $form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const $form = document.querySelector('.js-form-add-topic');
     const data = new FormData($form);
     const map = new Map(data.entries());
     console.log(map);
     
+    // 15) clear fields
     $form.reset();
+    
+    // 16) refresh list of templates
     renderTopics(map)
 
 });
 
-function renderTopics(map) {
+function renderTopics(mapElement) {
     
     const template2 = `
     <div class="column is-3">
             <div class="card">
                 <header class="card-header">
                     <p class="card-header-title">
-                        "Temat nr. 1"
+                        ${mapElement.get("topic")}
                     </p>
                 </header>
                 <div class="card-content">
                     <div class="content">
-                        Opis tematu...
+                        ${mapElement.get("description")}
                     </div>
                 </div>
                 <footer class="card-footer">
@@ -83,48 +116,7 @@ function renderTopics(map) {
         </div>
 
 `
+    // 18) add element
+    $jstopics.innerHTML += template2;
     
 }
-
-
-('submit', (e) => {
-    
-});
-
-
-
-
-
-$login.addEventListener('click', (e) => {
-
-    e.preventDefault();
-    hello('github').login()
-        .then(function () { 
-            return hello('github').api('/me');
-        })
-            .then(function (userProfile) {
-                renderUserDetails(userProfile);
-            })
-
-});
-
-
-function renderUserDetails(userProfile) {
-    const template = `
-        <div class="navbar-item">
-            ${userProfile.login}
-            ${userProfile.location}
-    
-            <img src="${userProfile.avatar_url}" alt=""/>
-        </div>  
-`
-    $user.innerHTML += template;
-    
-};
-
-
-$logout.addEventListener('click', () => {
-        console.log("logout");
-        hello.logout('github')
-            .then(() => location.reload(true))
-    });
